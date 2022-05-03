22c735d93d34ce807cf0a48687e45fd0f254c499
MinaC2 <103385793+MinaC2@users.noreply.github.com>
2022-05-03 21:05:47 +0000

    Sixth Project minor changes

--- MySixthProject/App copy.js
+++ MySixthProject/App copy.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default function App() {
 
 const csuLogo = {
-  uri: "https://raw.githubusercontent.com/ChaudharyMinahil/CIS340/master/images/rams.png",
+  uri: "https://images.squarespace-cdn.com/content/v1/56154fcae4b05391b2fa98b9/1576669046642-QLQVZLGX9N11R6MDJVXG/CSU+ram+horns.png?format=500w",
   width: 80,
   height: 80
 }
@@ -22,7 +22,7 @@ const csuLogo = {
 
       style = {styles.urlCSULogo}
       source = {{
-        uri: "https://raw.githubusercontent.com/ChaudharyMinahil/CIS340/master/images/logo.png"
+        uri: "https://raw.githubusercontent.com/ChaudharyMinahil/CIS340/master/images/Csulogo.png"
 
       }}
   />
